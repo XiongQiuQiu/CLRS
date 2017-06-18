aea5ac2278158ee75c2461b99d5e0a81b67d1f8e
XiongQiuQiu <zhangjinwei94@163.com>
2017-06-18 23:23:47 +0800

    c04

---
+++ C04/quicksort.py
@@ -0,0 +1,28 @@
+def partition(nums, p, q):
+    x = nums[p]
+    i = p
+    for j in range(p+1, q+1):
+        if nums[j] < x:
+            i += 1
+            nums[i], nums[j] = nums[j], nums[i]
+    nums[p], nums[i] = nums[i], nums[p]
+    return i
+
+
+def quicksort(nums, p, q):
+    '''
+    divide and conquer
+    sorts in place
+    very parctical
+    '''
+    if p < q:
+        k = partition(nums, p, q)
+        quicksort(nums, p, k-1)
+        quicksort(nums, k+1, p)
+    return nums
+
+if __name__ == '__main__':
+    nums = [6,10,13,5,8,3,2,11]
+    p = 0
+    q = len(nums) -1
+    quicksort(nums, p, q)
